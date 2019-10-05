aa98227caf6a996e60ace86327cd1ecb6029ec27
Aryan Ganotra <aryanganotra7@gmail.com>
2019-10-05 16:04:48 +0530

    Update ResultsDetail.js

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
 
 const ResultsDetail = ({result}) => {
     return (
         <View>
+            <Image style={styles.image} source={{ uri: result.thumb }}/>
             <Text>{result.name}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    image:{
+        width:250,
+        height:120,
+        borderRadius:4,
+
+    },
 
 });
 
